5f4f05ba46aac614628705b89a71e81bf9bed92b
林凤栩 <1731836246@qq.com>
2025-07-30 10:26:16 +0800

    修复日期对应问题

--- src/components/icons/realtimeCircularFigure.vue
+++ src/components/icons/realtimeCircularFigure.vue
@@ -174,8 +174,8 @@ export default {
   computed: {
     outerSeasonTexts() {
       return this.isStartLiChun  
-        ?['8月8，立秋','10月8，寒露','12月7，大雪','2月4，立春','4月5，清明','6月6，芒种'] 
-        :['7月23，大暑','9月23，秋分','11月22，小雪','1月21，大寒','3月21，春分','5月21，小满']
+        ?['8月4，立秋','10月4，寒露','12月4，大雪','2月4，立春','4月5，清明','6月4，芒种'] 
+        :['7月21，大暑','9月21，秋分','11月21，小雪','1月21，大寒','3月21，春分','5月21，小满']
     },
     outerColors() {
       return this.outerTexts.map(text => this.outerColorMap[text] || '#CCCCCC');
